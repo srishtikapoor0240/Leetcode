442c7af0a65af968520fe050d9e760d9cd8cca87
Srishti Kapoor <srishtikapoor0240@gmail.com>
2025-12-21 13:31:15 +0530

    Time: 8 ms (16.27%), Space: 48.1 MB (6.76%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        if(nums[nums.length-1]!=nums.length)
+            return nums.length;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(i!=nums[i])
+                return i;
+        }
+        return 0;
+    }
+}
