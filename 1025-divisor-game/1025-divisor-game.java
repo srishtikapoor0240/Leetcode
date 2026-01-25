45f8eccd99bba803db7dab45f348f5e61dc1e97d
Srishti Kapoor <srishtikapoor0240@gmail.com>
2026-01-25 22:03:48 +0530

    Time: 0 ms (100%), Space: 41.9 MB (77.04%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean divisorGame(int n) {
+        int c=1;
+        while(n>1){
+        for(int i=1;i<n;i++)
+        {
+            if(n%i==0) { 
+                n=n-i;
+                c++;
+                break;
+            }
+        }
+        }
+        return c%2==0;
+    }
+}
