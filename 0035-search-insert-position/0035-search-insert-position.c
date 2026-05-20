99b1f83cde9624225313206b16cf2ba31d3c6f99
Srishti Kapoor <srishtikapoor0240@gmail.com>
2026-05-20 19:22:50 +0530

    Time: 0 ms (100%), Space: 9.1 MB (31.72%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.c
+++ 0035-search-insert-position/0035-search-insert-position.c
@@ -2,20 +2,16 @@ int searchInsert(int* nums, int numsSize, int target) {
     int l=0;
     int h=numsSize-1;
     int mid;
-    while(l<=h)
-    {
-        mid=(l+h)/2;
-        if(nums[mid]==target)
+    while(l<=h){
+        mid =l+(h-l)/2;
+        if (target==nums[mid]){
             return mid;
-        else if(target<nums[mid])
+        }
+        else if(target<nums[mid]){
             h=mid-1;
-        else 
+        }
+        else if(target>nums[mid])
             l=mid+1;
     }
-    if(target<nums[0])
-        return 0;
-    if(target<nums[mid])
-        return mid;
-    else
-        return mid+1;
+    return l;
 }
