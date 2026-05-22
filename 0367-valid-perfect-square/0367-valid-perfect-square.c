dda6d41fd8f425d6512632e9b7608ae2f1d99b8f
Srishti Kapoor <srishtikapoor0240@gmail.com>
2026-05-23 05:28:59 +0530

    Time: 0 ms (100%), Space: 8.5 MB (59.13%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.c
+++ 0367-valid-perfect-square/0367-valid-perfect-square.c
@@ -1,9 +1,23 @@
-bool isPerfectSquare(int num) {
+/*bool isPerfectSquare(int num) {
     if (num==1)
         return true;
     for(long i=2;i<=num/2;i++){
         if (i*i==num)
             return true;
+    }
+    return false;
+}*/
+bool isPerfectSquare(int num){
+    int l=0;int h=num;
+    while(l<=h){
+        long mid=l+(h-l)/2;
+        if(mid*mid==num){
+            return true;
+        }
+        else if(mid*mid>num)
+            h=mid-1;
+        else if(mid*mid<num)
+            l=mid+1;
     }
     return false;
 }
