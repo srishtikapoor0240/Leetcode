baded403e10ebb77105636df85b2a6dc89d1637d
Srishti Kapoor <srishtikapoor0240@gmail.com>
2026-06-06 20:07:56 +0530

    Time: 0 ms (100%), Space: 51.8 MB (38.88%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,5 +1,5 @@
 //BRUTE FORCE 
-class Solution {
+/*class Solution {
     public int kthSmallest(int[][] matrix, int k) {
         int c=0;
         
@@ -15,10 +15,43 @@ class Solution {
         Arrays.sort(arr);
         return arr[k-1];
     }
-}
-
-/*class Solution {
-    public int kthSmallest(int[][] matrix, int k) {
-
-    }
 }*/
+
+//OPTIMAL SOL=> BINARY SERACH ON THE VALUES NOT THE INDICES
+class Solution {
+    public int count(int mid,int matrix[][]){
+        int n=matrix.length;
+        int r=0;
+        int c=n-1;
+        int count=0;
+        while (r<n && c>=0){
+            if(mid>=matrix[r][c]){
+                count+=c+1;
+                r++;
+            }
+            else{
+                c--;
+            }
+        }
+        return count;
+    }
+    public int kthSmallest(int[][] matrix, int k) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+
+        int l=matrix[0][0];
+        int h=matrix[n-1][m-1];
+
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(count(mid,matrix)<k){
+                l=mid+1;
+            }
+            else{
+                h=mid-1;
+            }
+            
+        }
+        return l;
+    }
+}
