2a8c1ff4e2af876e80883215c26d80a659673e99
Srishti Kapoor <srishtikapoor0240@gmail.com>
2026-06-08 13:38:01 +0530

    Time: 80 ms (0.41%), Space: 44.3 MB (17.81%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int findPeakElement(int[] nums) {
         int curr=nums[0];
         int index=0;
@@ -10,4 +10,22 @@ class Solution {
         }
         return index;
     }
+}*/
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int lo=0;
+        int hi=nums.length-2;
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            if(nums[mid]<nums[mid+1])
+            {
+                lo=mid+1;
+            }
+            else{
+                hi=mid-1;
+            }
+        }
+        return lo;
+    }
+
 }
