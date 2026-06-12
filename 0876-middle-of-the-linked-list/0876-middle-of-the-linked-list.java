22dec81592cd316780221eca2a6e70c17a35f55f
Srishti Kapoor <srishtikapoor0240@gmail.com>
2026-06-12 23:21:36 +0530

    Time: 0 ms (100%), Space: 42.6 MB (92.83%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -8,35 +8,8 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
-
-/*class Solution {
+class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode temp=head;
-        int c=0;
-        while(temp!=null)
-        {
-            
-            c++;
-            temp=temp.next;
-        }
-        temp=head;
-        int p=0;
-        while(p!=c/2){
-            temp=temp.next;
-            p++;
-
-        }
-        return temp;
-    }
-}
-*/
-
-//OPTIMAL SOL USING SLOW FAST POINTER
-
-class Solution 
-{
-    public ListNode middleNode(ListNode head){
         ListNode slow=head;
         ListNode fast=head;
 
