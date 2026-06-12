c1986305c4b0e64916a5a19dc38869cb940b9cf5
Srishti Kapoor <srishtikapoor0240@gmail.com>
2026-06-12 23:16:23 +0530

    Time: 0 ms (100%), Space: 46.7 MB (71.24%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,17 +11,14 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode temp=head;
-        
         ListNode slow=head;
         ListNode fast=head;
-
         while(fast!=null && fast.next!=null){
-            slow=slow.next;
+            slow= slow.next;
             fast=fast.next.next;
-            if(slow==fast)
+            if(slow==fast){
                 return true;
-            
+            }
         }
         return false;
     }
