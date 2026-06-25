183b07d42456470ca8b6a698a6f6e4ebae8a43d5
Srishti Kapoor <srishtikapoor0240@gmail.com>
2026-06-25 20:20:28 +0530

    Time: 4 ms (74.54%), Space: 44.9 MB (76.35%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,4 +1,5 @@
-class Solution {
+//BRUTE FORCE 
+/*class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int ans[]=new int[nums1.length];     
         for(int i=0;i<nums1.length;i++){
@@ -19,6 +20,30 @@ class Solution {
                 ans[i]=-1;
             }
             
+        }
+        return ans;
+    }
+}*/
+
+// OPTIMAL USING MONOTONIC STACK
+class Solution{
+    public int[] nextGreaterElement(int []nums1,int[]nums2){
+        Stack <Integer>st=new Stack<>();
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<nums2.length;i++){
+            while(!st.isEmpty() && nums2[i]>st.peek()){
+                map.put(st.pop(),nums2[i]);
+            }
+            st.push(nums2[i]);
+        }
+        while(!st.isEmpty()) {
+            map.put(st.pop(), -1);
+        }
+
+        int[] ans = new int[nums1.length];
+
+        for(int i = 0; i < nums1.length; i++) {
+            ans[i] = map.get(nums1[i]);
         }
         return ans;
     }
