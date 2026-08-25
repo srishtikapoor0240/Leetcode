da6e30347bc9b9391e754060a9162939ab976c99
Srishti Kapoor <srishtikapoor0240@gmail.com>
2026-08-25 17:36:06 +0530

    Time: 2 ms (95.79%), Space: 48.6 MB (70.34%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,9 +1,27 @@
+/*class Solution {
+    public int[] countBits(int n) {
+        int ans[]=new int[n+1];
+        for(int i=0;i<ans.length;i++){
+            int c=0;
+            int x=i;
+            while(x!=0){
+                int s=x&1;
+                if (s==1)
+                    c++;
+                x=x>>1;
+            }
+            ans[i]=c;
+        }
+        return ans;
+    }   
+}*/
 class Solution {
     public int[] countBits(int n) {
+
         int[] ans = new int[n + 1];
 
         for (int i = 1; i <= n; i++) {
-            ans[i] = ans[i & (i - 1)] + 1;
+            ans[i] = ans[i >> 1] + (i & 1);
         }
 
         return ans;
