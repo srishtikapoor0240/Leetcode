cf4255443517b6bd38f784e29c5685a58098d140
Srishti Kapoor <srishtikapoor0240@gmail.com>
2026-08-27 20:28:24 +0530

    Time: 0 ms (100%), Space: 42.6 MB (18.25%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -15,7 +15,7 @@ class Solution {
         int ans=0;
         for(int i=0;i<32;i++){
             ans=(ans<<1)| (n&1);
-            n=n>>>1;
+            n=n/2;
         }
         return ans;
     }
