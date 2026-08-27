593fab3037b338a2736e7d91e7c43d1f7d4f354f
Srishti Kapoor <srishtikapoor0240@gmail.com>
2026-08-27 19:54:42 +0530

    Time: 0 ms (100%), Space: 47.3 MB (48.99%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int missingNumber(int[] nums) {
         int sum=0;
         for(int i=0;i<=nums.length;i++)
@@ -8,5 +8,15 @@ class Solution {
             miss+=nums[i];
         }
         return sum-miss;
+    }
+}*/
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor=nums.length;
+        for(int i=0;i<nums.length;i++){
+            xor=xor^i;
+            xor=xor^nums[i];
+        }
+        return xor;
     }
 }
