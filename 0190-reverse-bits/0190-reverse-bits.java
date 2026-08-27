89dc8e836294b4a96707cadf9b129a45327b457a
Srishti Kapoor <srishtikapoor0240@gmail.com>
2026-08-27 20:18:59 +0530

    Time: 1 ms (61.08%), Space: 42.6 MB (18.25%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int reverseBits(int n) {
         String s =Integer.toBinaryString(n);
         while(s.length()<32){
@@ -8,5 +8,15 @@ class Solution {
         
         long r=Long.parseLong(rev,2);
         return (int)r;
+    }
+}*/
+class Solution {
+    public int reverseBits(int n) {
+        int ans=0;
+        for(int i=0;i<32;i++){
+            ans=(ans<<1)| (n&1);
+            n=n>>>1;
+        }
+        return ans;
     }
 }
